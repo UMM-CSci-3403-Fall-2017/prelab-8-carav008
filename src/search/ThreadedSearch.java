89e2e60dc7a27484fe529a7f7b4d8c9770493abd
carav008 <carav008@morris.umn.edu>
2017-11-07 21:47:40 -0600

    Finish implementing 'ThreadedSearch.java'
    Checks pass in main as compared to with the Linear search file

--- src/search/ThreadedSearch.java
+++ src/search/ThreadedSearch.java
@@ -4,35 +4,37 @@ import java.util.ArrayList;
 
 public class ThreadedSearch<T> implements Runnable {
 
-  private T target;
-  private ArrayList<T> list;
-  private int begin;
-  private int end;
-  private Answer answer;
+    private T target;
+    private ArrayList<T> list;
+    private int begin;
+    private int end;
+    private Answer answer;
 
-  public ThreadedSearch() {
-  }
+    public ThreadedSearch() {
+    }
 
-  private ThreadedSearch(T target, ArrayList<T> list, int begin, int end, Answer answer) {
-    this.target=target;
-    this.list=list;
-    this.begin=begin;
-    this.end=end;
-    this.answer=answer;
-  }
+    private ThreadedSearch(T target, ArrayList<T> list, int begin, int end, Answer answer) {
+        this.target=target;
+        this.list=list;
+        this.begin=begin;
+        this.end=end;
+        this.answer=answer;
+    }
 
-  /**
-  * Searches `list` in parallel using `numThreads` threads.
-  *
-  * You can assume that the list size is divisible by `numThreads`
-  */
-  public boolean parSearch(int numThreads, T target, ArrayList<T> list) throws InterruptedException {
+    /**
+     * Searches `list` in parallel using `numThreads` threads.
+     *
+     * You can assume that the list size is divisible by `numThreads`
+     */
+    public boolean parSearch(int numThreads, T target, ArrayList<T> list) throws InterruptedException {
     /*
     * First construct an instance of the `Answer` inner class. This will
     * be how the threads you're about to create will "communicate". They
     * will all have access to this one shared instance of `Answer`, where
     * they can update the `answer` field inside that instance.
-    *
+    */
+        Answer answer = new Answer();
+    /*
     * Then construct `numThreads` instances of this class (`ThreadedSearch`)
     * using the 5 argument constructor for the class. You'll hand each of
     * them the same `target`, `list`, and `answer`. What will be different
@@ -41,31 +43,53 @@ public class ThreadedSearch<T> implements Runnable {
     * of the list. If, for example, the list has length 100 and you have
     * 4 threads, you would give the four threads the ranges [0, 25), [25, 50),
     * [50, 75), and [75, 100) as their sections to search.
-    *
+    */
+
+        Thread[] threads = new Thread[numThreads];
+        int range = list.size() / numThreads;
+        int start = 0;
+        for (int i = 0; i < numThreads;++i){
+            threads[i] = new Thread(new ThreadedSearch(target,list,start,start + range,answer));
+            start = start + range;
+            threads[i].start();
+        }
+
+    /*
     * You then construct `numThreads`, each of which is given a different
     * instance of this class as its `Runnable`. Then start each of those
     * threads, wait for them to all terminate, and then return the answer
     * in the shared `Answer` instance.
     */
-    return false;
-  }
+        for(int i = 0; i < numThreads; ++i){
+            threads[i].join();
 
-  public void run() {
+        }
 
-  }
-
-  private class Answer {
-    private boolean answer = false;
-
-    public boolean getAnswer() {
-      return answer;
+        return answer.getAnswer();
     }
 
-    // This has to be synchronized to ensure that no two threads modify
-    // this at the same time, possibly causing race conditions.
-    public synchronized void setAnswer(boolean newAnswer) {
-      answer = newAnswer;
+    public void run() {
+        for(int i = 0; i < list.size();i++){
+            //Check if the search returns the result we want
+            if(list.get(i).equals(target)){
+                this.answer.setAnswer(true);
+                break;
+            }
+        }
     }
-  }
+
+    private class Answer {
+        private boolean answer = false;
+
+        public boolean getAnswer() {
+            return answer;
+        }
+
+        // This has to be synchronized to ensure that no two threads modify
+        // this at the same time, possibly causing race conditions.
+        public synchronized void setAnswer(boolean newAnswer) {
+            answer = newAnswer;
+        }
+    }
 
 }
